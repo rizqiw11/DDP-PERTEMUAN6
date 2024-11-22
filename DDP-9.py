ed8b713521887bb8792946449b3fda9b8d8d4e2b
rizqiw11 <rizkifajarmaulana78@gmail.com>
2024-11-22 13:24:01 +0700

    Add files via upload

--- DDP-9.py
+++ DDP-9.py
@@ -1,1 +1,34 @@
-print("halo")
+print('---- mencari celcius ke fahrenheit ---- ')
+def celcius_ke_fahrenheit(celcius):
+    fahrenheit = (celcius * 9/5) + 32
+    return fahrenheit 
+
+print(celcius_ke_fahrenheit(0))
+print(celcius_ke_fahrenheit(100))
+
+print('\n---- mencari bilangan genap ----')
+def is_genap(bil_genap):
+    return bil_genap %2 == 0
+# untuk memasukan value
+print(is_genap(4))
+print(is_genap(7))
+
+print('\n---- mencetak nilai kelulusan ----')
+# rata" nilai 70
+def nilai_kelulusan(nilai):
+    if nilai >= 80:
+        return 'lulus'
+    else :
+        return 'gagal'
+     
+     
+#untuk mencetak value 
+print(nilai_kelulusan(80))
+print(nilai_kelulusan(60))
+
+print('\n---- cetak bilangan ganjil ----')
+def bil_ganjil(angka):
+    for i in range(1, angka, 2):
+        print(i, end=" ")
+#untuk memasukan value
+bil_ganjil(20)
